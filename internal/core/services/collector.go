da66c879e3581358850b68be1efdd30156d8dbfa
Shuhrat <shukhrat.shokirov@alif.uz>
2024-10-29 20:20:56 +0500

    fix by lint

--- internal/core/services/collector.go
+++ internal/core/services/collector.go
@@ -23,32 +23,35 @@ func (c *Collector) CollectMetrics() []model.Metric {
 	// Увеличиваем PollCount на 1 при каждом опросе метрик
 	atomic.AddInt64(&c.pollCount, 1)
 
-	return []model.Metric{
-		{"Alloc", float64(memStats.Alloc), "gauge"},
-		{"BuckHashSys", float64(memStats.BuckHashSys), "gauge"},
-		{"Frees", float64(memStats.Frees), "gauge"},
-		{"GCCPUFraction", float64(memStats.GCCPUFraction), "gauge"},
-		{"HeapAlloc", float64(memStats.HeapAlloc), "gauge"},
-		{"HeapIdle", float64(memStats.HeapIdle), "gauge"},
-		{"HeapInuse", float64(memStats.HeapInuse), "gauge"},
-		{"HeapObjects", float64(memStats.HeapObjects), "gauge"},
-		{"HeapReleased", float64(memStats.HeapReleased), "gauge"},
-		{"HeapSys", float64(memStats.HeapSys), "gauge"},
-		{"LastGC", float64(memStats.LastGC), "gauge"},
-		{"Lookups", float64(memStats.Lookups), "gauge"},
-		{"Mallocs", float64(memStats.Mallocs), "gauge"},
-		{"MCacheInuse", float64(memStats.MCacheInuse), "gauge"},
-		{"MCacheSys", float64(memStats.MCacheSys), "gauge"},
-		{"MSpanInuse", float64(memStats.MSpanInuse), "gauge"},
-		{"MSpanSys", float64(memStats.MSpanSys), "gauge"},
-		{"NextGC", float64(memStats.NextGC), "gauge"},
-		{"NumGC", float64(memStats.NumGC), "gauge"},
-		{"PauseTotalNs", float64(memStats.PauseTotalNs), "gauge"},
-		{"StackInuse", float64(memStats.StackInuse), "gauge"},
-		{"StackSys", float64(memStats.StackSys), "gauge"},
-		{"Sys", float64(memStats.Sys), "gauge"},
-		{"TotalAlloc", float64(memStats.TotalAlloc), "gauge"},
-		{"PollCount", float64(atomic.LoadInt64(&c.pollCount)), "counter"}, // PollCount
-		{"RandomValue", rand.Float64() * 100, "gauge"},                    // RandomValue
-	}
+	// Собираем метрики
+	metrics := make([]model.Metric, 0, 24)
+
+	metrics = append(metrics, model.Metric{Name: "Alloc", Value: float64(memStats.Alloc), Type: "gauge"})
+	metrics = append(metrics, model.Metric{Name: "BuckHashSys", Value: float64(memStats.BuckHashSys), Type: "gauge"})
+	metrics = append(metrics, model.Metric{Name: "Frees", Value: float64(memStats.Frees), Type: "gauge"})
+	metrics = append(metrics, model.Metric{Name: "GCCPUFraction", Value: memStats.GCCPUFraction, Type: "gauge"})
+	metrics = append(metrics, model.Metric{Name: "HeapAlloc", Value: float64(memStats.HeapAlloc), Type: "gauge"})
+	metrics = append(metrics, model.Metric{Name: "HeapIdle", Value: float64(memStats.HeapIdle), Type: "gauge"})
+	metrics = append(metrics, model.Metric{Name: "HeapInuse", Value: float64(memStats.HeapInuse), Type: "gauge"})
+	metrics = append(metrics, model.Metric{Name: "HeapObjects", Value: float64(memStats.HeapObjects), Type: "gauge"})
+	metrics = append(metrics, model.Metric{Name: "HeapReleased", Value: float64(memStats.HeapReleased), Type: "gauge"})
+	metrics = append(metrics, model.Metric{Name: "HeapSys", Value: float64(memStats.HeapSys), Type: "gauge"})
+	metrics = append(metrics, model.Metric{Name: "LastGC", Value: float64(memStats.LastGC), Type: "gauge"})
+	metrics = append(metrics, model.Metric{Name: "Lookups", Value: float64(memStats.Lookups), Type: "gauge"})
+	metrics = append(metrics, model.Metric{Name: "Mallocs", Value: float64(memStats.Mallocs), Type: "gauge"})
+	metrics = append(metrics, model.Metric{Name: "MCacheInuse", Value: float64(memStats.MCacheInuse), Type: "gauge"})
+	metrics = append(metrics, model.Metric{Name: "MCacheSys", Value: float64(memStats.MCacheSys), Type: "gauge"})
+	metrics = append(metrics, model.Metric{Name: "MSpanInuse", Value: float64(memStats.MSpanInuse), Type: "gauge"})
+	metrics = append(metrics, model.Metric{Name: "MSpanSys", Value: float64(memStats.MSpanSys), Type: "gauge"})
+	metrics = append(metrics, model.Metric{Name: "NextGC", Value: float64(memStats.NextGC), Type: "gauge"})
+	metrics = append(metrics, model.Metric{Name: "NumGC", Value: float64(memStats.NumGC), Type: "gauge"})
+	metrics = append(metrics, model.Metric{Name: "PauseTotalNs", Value: float64(memStats.PauseTotalNs), Type: "gauge"})
+	metrics = append(metrics, model.Metric{Name: "StackInuse", Value: float64(memStats.StackInuse), Type: "gauge"})
+	metrics = append(metrics, model.Metric{Name: "StackSys", Value: float64(memStats.StackSys), Type: "gauge"})
+	metrics = append(metrics, model.Metric{Name: "Sys", Value: float64(memStats.Sys), Type: "gauge"})
+	metrics = append(metrics, model.Metric{Name: "TotalAlloc", Value: float64(memStats.TotalAlloc), Type: "gauge"})
+	metrics = append(metrics, model.Metric{Name: "PollCount", Value: float64(atomic.LoadInt64(&c.pollCount)), Type: "counter"})
+	metrics = append(metrics, model.Metric{Name: "RandomValue", Value: rand.Float64() * 100, Type: "gauge"})
+
+	return metrics
 }
